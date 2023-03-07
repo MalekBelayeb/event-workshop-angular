89f47ecca4d248c01774dcb3b654b2d8caa7083e
MalekBelayeb <malek.belayeb@esprit.tn>
2023-03-07 09:51:34 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path: 'login', component:LoginComponent},
+  //{path: 'register',component:RegisterComponent},
+  //{path: 'reset-password',component:ResetpasswordComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
