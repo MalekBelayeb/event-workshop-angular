a3fb69cf8a2fd4e415734354339c22e77831e63a
MalekBelayeb <malek.belayeb@esprit.tn>
2023-03-08 09:32:55 +0100

    feat: authentification layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './authentification/register/register.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
 const routes: Routes = [
-  //{path: 'login', component:LoginComponent},
-  //{path: 'register',component:RegisterComponent},
-  //{path: 'reset-password',component:ResetpasswordComponent},
+  
+  {path: '', redirectTo:'/home',pathMatch:'full'},
+  {path: 'auth', component:AuthentificationComponent},
+  {path: 'register',component:RegisterComponent},
+  {path: 'home',component:HomeComponent},
+  {path: '**',component:ErrorPageComponent},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './authentification/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './authentification/register/register.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs'; 
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    ErrorPageComponent,
+    AuthentificationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
